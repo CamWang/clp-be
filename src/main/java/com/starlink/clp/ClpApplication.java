b5ad20fb864c230c4ea26e817ba7e739443ef943
Cam Wang <camwang@outlook.com>
2020-07-24 12:07:19 +0800

    Initial Commit
    Project environment and basic settings.

---
+++ src/main/java/com/starlink/clp/ClpApplication.java
@@ -0,0 +1,13 @@
+package com.starlink.clp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/starlink/clp/ClpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starlink.clp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
